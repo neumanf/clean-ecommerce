9f496300f1ec0bee29ada26f3fb3486f8881576b
neumanf <fabricionewman@protonmail.com>
2022-04-24 11:52:02 +0000

    feat: add status code and data fields to any response

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function bootstrap() {
 
     app.setGlobalPrefix('api');
     app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalInterceptors(new TransformInterceptor());
 
     await app.listen(configService.get('port'));
 }
