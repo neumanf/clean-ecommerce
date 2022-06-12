9d2566819ee7e4dbf5f31235e3da251262dc4653
neumanf <fabricionewman@protonmail.com>
2022-06-12 12:21:12 -0300

    feat: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     app.setGlobalPrefix('api');
     app.useGlobalPipes(new ValidationPipe());
     app.useGlobalInterceptors(new TransformInterceptor());
+    app.enableCors();
 
     await app.listen(configService.get('port'));
 }
