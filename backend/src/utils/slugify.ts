bb76d4b083060453f594ff69f72eedabe67e0f48
neumanf <fabricionewman@protonmail.com>
2022-06-15 19:32:52 -0300

    feat: add short cuid to the product slug

--- backend/src/utils/slugify.ts
+++ backend/src/utils/slugify.ts
@@ -1,5 +1,8 @@
+import * as cuid from 'cuid';
+
 export const slugify = (str: string) =>
     str
         .toLowerCase()
         .replace(/\s+/g, '-')
-        .replace(/[^\w-]+/g, '');
+        .replace(/[^\w-]+/g, '')
+        .concat('-', cuid.slug());
